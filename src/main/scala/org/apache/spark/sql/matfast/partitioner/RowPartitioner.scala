e6da26fe43f142be76806702935225833efd335b
Yongyang Yu <yuyongyang800@gmail.com>
2017-08-14 14:25:48 -0400

    update code style for row partitioner source

--- src/main/scala/org/apache/spark/sql/matfast/partitioner/RowPartitioner.scala
+++ src/main/scala/org/apache/spark/sql/matfast/partitioner/RowPartitioner.scala
@@ -1,3 +1,20 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.apache.spark.sql.matfast.partitioner
 
 import org.apache.spark.{Partitioner, SparkConf}
@@ -6,14 +23,11 @@ import org.apache.spark.sql.catalyst.InternalRow
 import org.apache.spark.sql.matfast.util.MatfastSerializer
 
 
-/**
-  * Created by yongyangyu on 3/13/17.
-  */
 class RowPartitioner(partitions: Int) extends Partitioner{
 
   require(partitions >= 0, s"Number of partitions cannot be negative but found $partitions")
 
-  override def numPartitions = partitions
+  override val numPartitions = partitions
 
   override def getPartition(key: Any): Int = {
     key match {
@@ -26,6 +40,10 @@ class RowPartitioner(partitions: Int) extends Partitioner{
   override def equals(other: Any): Boolean = {
     other.isInstanceOf[RowPartitioner] &&
       numPartitions == other.asInstanceOf[RowPartitioner].numPartitions
+  }
+
+  override def hashCode(): Int = {
+    com.google.common.base.Objects.hashCode(partitions: java.lang.Integer)
   }
 }
 

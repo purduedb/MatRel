f70d5c0edbf8e962a125b6e856d2c1c66f8b923c
Yongyang Yu <yuyongyang800@gmail.com>
2017-08-14 13:38:20 -0400

    update code style for matfast plan source

--- src/main/scala/org/apache/spark/sql/matfast/execution/MatfastPlan.scala
+++ src/main/scala/org/apache/spark/sql/matfast/execution/MatfastPlan.scala
@@ -1,14 +1,30 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.apache.spark.sql.matfast.execution
 
-import org.apache.spark.sql.matfast.MatfastSession
 import org.apache.spark.sql.execution.SparkPlan
+import org.apache.spark.sql.matfast.MatfastSession
 
-/**
-  * Created by yongyangyu on 11/29/16.
-  */
+
 abstract class MatfastPlan extends SparkPlan{
   @transient
-  protected[matfast] final val matfastSessionState = MatfastSession.getActiveSession.map(_.sessionState).orNull
+  protected[matfast] final val matfastSessionState =
+    MatfastSession.getActiveSession.map(_.sessionState).orNull
 
   protected override def sparkContext = MatfastSession.getActiveSession.map(_.sparkContext).orNull
 }

636a3bad5825d3e49668402fa7195c22a1513624
Yongyang Yu <yuyongyang800@gmail.com>
2017-08-14 13:39:53 -0400

    update code style for query execution source

--- src/main/scala/org/apache/spark/sql/matfast/execution/QueryExecution.scala
+++ src/main/scala/org/apache/spark/sql/matfast/execution/QueryExecution.scala
@@ -1,13 +1,29 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.apache.spark.sql.matfast.execution
 
 import org.apache.spark.sql.catalyst.plans.logical.LogicalPlan
-import org.apache.spark.sql.execution.{SparkPlan, QueryExecution => SQLQueryExecution}
+import org.apache.spark.sql.execution.{QueryExecution => SQLQueryExecution, SparkPlan}
 import org.apache.spark.sql.matfast.MatfastSession
-/**
-  * Created by yongyangyu on 11/29/16.
-  */
+
+
 class QueryExecution(val matfastSession: MatfastSession, val matfastLogical: LogicalPlan)
-  extends SQLQueryExecution(matfastSession, matfastLogical){
+  extends SQLQueryExecution(matfastSession, matfastLogical) {
 
   lazy val matrixData: LogicalPlan = {
     assertAnalyzed()
@@ -15,7 +31,8 @@ class QueryExecution(val matfastSession: MatfastSession, val matfastLogical: Log
   }
 
   override lazy val optimizedPlan: LogicalPlan = {
-    matfastSession.sessionState.matfastOptimizer.execute(matfastSession.sessionState.getSQLOptimizer.execute(matrixData))
+    matfastSession.sessionState.matfastOptimizer.execute(
+      matfastSession.sessionState.getSQLOptimizer.execute(matrixData))
   }
 
   override lazy val sparkPlan: SparkPlan = {

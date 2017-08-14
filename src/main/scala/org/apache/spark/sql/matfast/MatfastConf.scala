63b8d09399c6ac96932e1d7f04572a735a6350f5
Yongyang Yu <yuyongyang800@gmail.com>
2017-08-14 14:44:25 -0400

    update code style for matfast conf

--- src/main/scala/org/apache/spark/sql/matfast/MatfastConf.scala
+++ src/main/scala/org/apache/spark/sql/matfast/MatfastConf.scala
@@ -1,12 +1,26 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.apache.spark.sql.matfast
 import java.util.Properties
 
 import scala.collection.JavaConverters._
 import scala.collection.immutable
 
-/**
-  * Created by yongyangyu on 2/16/17.
-  */
 
 private[matfast] object MatfastConf {
   private val matfastConfEntries = java.util.Collections.synchronizedMap(
@@ -22,16 +36,19 @@ private[matfast] object MatfastConf {
     def defaultValueString: String = defaultValue.map(stringConverter).getOrElse("<undefined>")
 
     override def toString: String = {
-      s"MatfastConfEntry(key = $key, defaultValue = $defaultValueString, doc = $doc, isPublic = $isPublic)"
+      s"MatfastConfEntry(key = $key, defaultValue = $defaultValueString, " +
+        s"doc = $doc, isPublic = $isPublic)"
     }
   }
 
   private[matfast] object MatfastConfEntry {
     private def apply[T](key: String, defaultValue: Option[T], valueConverter: String => T,
-                         stringConverter: T => String, doc: String, isPublic: Boolean): MatfastConfEntry[T] =
+                         stringConverter: T => String,
+                         doc: String, isPublic: Boolean): MatfastConfEntry[T] =
       matfastConfEntries.synchronized {
         if (matfastConfEntries.containsKey(key)) {
-          throw new IllegalArgumentException(s"Duplicate MatfastConfEntry. $key has been registered")
+          throw new IllegalArgumentException(
+            s"Duplicate MatfastConfEntry. $key has been registered")
         }
         val entry =
           new MatfastConfEntry[T](key, defaultValue, valueConverter, stringConverter, doc, isPublic)

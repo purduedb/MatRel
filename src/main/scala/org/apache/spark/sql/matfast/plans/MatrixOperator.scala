87e8802fe14db6d355e1f3d13248f154a0c52263
Yongyang Yu <yuyongyang800@gmail.com>
2017-08-14 12:58:23 -0400

    update code style for matrix operator source

--- src/main/scala/org/apache/spark/sql/matfast/plans/MatrixOperator.scala
+++ src/main/scala/org/apache/spark/sql/matfast/plans/MatrixOperator.scala
@@ -1,11 +1,25 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.apache.spark.sql.matfast.plans
 
 import org.apache.spark.sql.catalyst.expressions.{Attribute}
 import org.apache.spark.sql.catalyst.plans.logical.{BinaryNode, LogicalPlan, UnaryNode}
 
-/**
-  * Created by yongyangyu on 2/23/17.
-  */
 case class TranposeOperator(child: LogicalPlan) extends UnaryNode {
   override def output: Seq[Attribute] = child.output
 }
@@ -45,7 +59,8 @@ case class MatrixPowerOperator(child: LogicalPlan, scalar: Double) extends Unary
 }
 
 // stack the elements of a matrix in a column fashion
-case class VectorizeOperator(child: LogicalPlan, nrows: Long, ncols: Long, blkSize: Int) extends UnaryNode {
+case class VectorizeOperator(child: LogicalPlan, nrows: Long,
+                             ncols: Long, blkSize: Int) extends UnaryNode {
   override def output: Seq[Attribute] = child.output
 }
 

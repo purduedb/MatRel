52a7931be635826f96efb20930d737be73f75d81
Yongyang Yu <yuyongyang800@gmail.com>
2017-08-14 14:42:50 -0400

    update code style for matfast session

--- src/main/scala/org/apache/spark/sql/matfast/MatfastSession.scala
+++ src/main/scala/org/apache/spark/sql/matfast/MatfastSession.scala
@@ -1,24 +1,39 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.apache.spark.sql.matfast
 
 import java.util.concurrent.atomic.AtomicReference
+
+import scala.reflect.ClassTag
+import scala.util.control.NonFatal
 
 import org.apache.spark.{SparkConf, SparkContext}
 import org.apache.spark.annotation.InterfaceStability
 import org.apache.spark.internal.Logging
 import org.apache.spark.scheduler.{SparkListener, SparkListenerApplicationEnd}
+import org.apache.spark.sql.{Dataset => SparkDataSet}
 import org.apache.spark.sql.execution.ui.SQLListener
 import org.apache.spark.sql.internal.SessionState
 import org.apache.spark.sql.internal.StaticSQLConf._
 import org.apache.spark.sql.{SparkSession => SParkSparkSession, _}
-import org.apache.spark.sql.{Dataset => SparkDataSet}
 import org.apache.spark.util.Utils
 
-import scala.reflect.ClassTag
-import scala.util.control.NonFatal
 
-/**
-  * Created by yongyangyu on 2/17/17.
-  */
 class MatfastSession private[matfast]
 (@transient override val sparkContext: SparkContext) extends SParkSparkSession(sparkContext)
 {
